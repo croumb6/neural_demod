f4a776d434553d3d5ddf2cfb41de29125b44e6d3
Michael Crouse <croumb6@gmail.com>
2017-10-27 14:17:29 -0400

    generation correction for old way

--- data_gen/generate_qpsk.py
+++ data_gen/generate_qpsk.py
@@ -3,7 +3,7 @@ from transmitters import *
 import analyze_stats
 from gnuradio import channels, gr, blocks
 import numpy as np
-import numpy.fft, cPickle, gzip
+import numpy.fft, gzip
 import random
 import matplotlib.pyplot as plt
 plt.ion()
@@ -23,86 +23,95 @@ dataset = {}
 
 transmitters = {
     "discrete":[transmitter_qpsk, transmitter_8psk, transmitter_pam4, transmitter_qam16, transmitter_qam64, transmitter_gfsk, transmitter_cpfsk],
-    # "discrete":[transmitter_bpsk, transmitter_qpsk, transmitter_8psk, transmitter_pam4, transmitter_qam16, transmitter_qam64, transmitter_gfsk, transmitter_cpfsk],
-    "continuous":[transmitter_fm, transmitter_am, transmitter_amssb]
     }
+
 nvecs_per_key = 1000
 vec_length = 128
 snr_vals = range(-20,20,2)
-snr_vals = [14]
 # this is based on 2 bits per symbol and 100 '00' padding on both sides of random data
 start_idx = 1148#/bps
 bps = 2
+mod_type = transmitter_qpsk
+print mod_type.modname
+
 for snr in snr_vals:
     print "snr is ", snr
-    for alphabet_type in transmitters.keys():
-        for i,mod_type in enumerate(transmitters[alphabet_type]):
-          dataset[(mod_type.modname, snr)] = np.zeros([nvecs_per_key, 2, vec_length], dtype=np.float32)
-          print mod_type.modname
-          # moar vectors!
-          insufficient_modsnr_vectors = True
-          modvec_indx = 0
-          while insufficient_modsnr_vectors:
-              tx_len = int(10e3)
-              if mod_type.modname == "QAM16":
-                  tx_len = int(20e3)
-              if mod_type.modname == "QAM64":
-                  tx_len = int(30e3)
-              #src = source_alphabet(alphabet_type, tx_len, True)
-              #   vecs = vecs.append(np.tile(np.array([0]*10 + [1]*10), tx_len/2))
-              np.random.seed(8)
-              rand_bits = np.random.randint(0,2, tx_len)
-              #vecs = np.concatenate((np.zeros(100), np.array([0,0, 0,1, 1,0,1,1, 0, 1]), np.zeros(100)))
-              vecs = np.concatenate((np.zeros(100*bps), rand_bits, np.zeros(100*bps)))
-              
-              src = blocks.vector_source_b(map(int, vecs), False)
-              mod = mod_type()
-              fD = 1
-              delays = [0.0, 0.9, 1.7]
-              mags = [1, 0.8, 0.3]
-              ntaps = 8
-              noise_amp = 10**(-snr/10.0)
-              chan = channels.dynamic_channel_model( 200e3, 0.01, 50, .01, 0.5e3, 8, fD, True, 4, delays, mags, ntaps, noise_amp, 0x1337 )
+    
+        # moar vectors!
 
-              snk = blocks.vector_sink_c()
-            #   snk = blocks.vector_sink_b(8)
+    tx_len = int(10e4)
+    if mod_type.modname == "QAM16":
+        tx_len = int(20e3)
+    if mod_type.modname == "QAM64":
+        tx_len = int(30e3)
+    #src = source_alphabet(alphabet_type, tx_len, True)
+    #   vecs = vecs.append(np.tile(np.array([0]*10 + [1]*10), tx_len/2))
+    np.random.seed(8)
+    rand_bits = np.random.randint(0,2, tx_len)
+    #vecs = np.concatenate((np.zeros(100), np.array([0,0, 0,1, 1,0,1,1, 0, 1]), np.zeros(100)))
+    vecs = np.concatenate((np.zeros(100*bps), rand_bits, np.zeros(100*bps)))
+    
+    src = blocks.vector_source_b(map(int, vecs), False)
+    mod = mod_type()
+    fD = 1
+    delays = [0.0, 0.9, 1.7]
+    mags = [1, 0.8, 0.3]
+    ntaps = 8
+    
+    # print taps
+    noise_amp = 10**(-snr/10.0)
 
-              tb = gr.top_block()
+    #samp_rate, sro_std_dev, sro_max_dev, cfo_std_dev, cfo_max_dev, N, doppler, LOS_model, K, delays, mags, ntaps_mpath, noise_amp, nosie_seed
+    # chan = channels.dynamic_channel_model( 200e3, 0.01, 50, .01, 0.5e3, 8, fD, True, 4, delays, mags, ntaps, noise_amp, 0x1337 )
 
-              # connect blocks
-              if apply_channel:
-                  tb.connect(src, mod, chan, snk)
-              else:
-                  tb.connect(src, mod, snk)
-              tb.run()
+    #noise_voltage, freq_off, epsilon, taps, noise_seed, block_tags
+    freq_off = 0
+    taps = [1.0, 0.25-0.25j, 0.50 + 0.10j, -0.3 + 0.2j]
+    np.random.seed(10)
+    # taps = np.random.random(ntaps-1) + 1j*np.random.random(ntaps-1)
+    # taps = np.concatenate((np.ones(1), taps))
+    np.random.seed()
+    eps = 1
+    chan = channels.channel_model(noise_amp, freq_off,  eps, taps)
 
+    snk = blocks.vector_sink_c()
+#   snk = blocks.vector_sink_b(8)
 
-              vecs_out = np.array(snk.data())
+    tb = gr.top_block()
 
-            #   wind_size = 16 
-              wind_size = 128 
-              sym_len = 8  # also symbol length
-              idx = np.arange(0, wind_size)
-              x = vecs_out[start_idx:start_idx+ (sym_len * (rand_bits.shape[0]/2))] 
-              x_patched = x[np.array([idx + i for i in range(0, len(x)-(wind_size-sym_len), sym_len)])]
-
-              vecs_real = vecs[200:-200]
-              tmp = vecs_real.reshape(-1,2)
-              keys = np.unique(tmp, axis=0)
-              sym_l = np.zeros(tmp.shape[0])
-              for i, k in enumerate(keys):
-                  sym_l[np.where((tmp == k).all(axis=1))] = i
+    # connect blocks
+    if apply_channel:
+        tb.connect(src, mod, chan, snk)
+    else:
+        tb.connect(src, mod, snk)
+    tb.run()
 
 
-              idx = np.arange(0, wind_size/sym_len)
-              syms_patched = sym_l[np.array([idx + i for i in range(0, len(sym_l)-(wind_size/sym_len)+1, 1)])]
+    vecs_out = np.array(snk.data())
 
-              np.save('qpsk_snr%d_iq_%d.npy' % (snr, wind_size), x_patched)
-              np.save('qpsk_snr%d_syms_%d.npy' % (snr,wind_size/sym_len), syms_patched)
+#   wind_size = 16 
+    wind_size = 128 
+    sym_len = 8  # also symbol length
+    idx = np.arange(0, wind_size)
+    x = vecs_out[start_idx:start_idx+ (sym_len * (rand_bits.shape[0]/2))] 
+    x = x/np.sum(np.abs(x))
+    # x = (x - np.min(x))/(np.max(x) - np.min(x))
+    # x_patched = x[np.array([idx + i for i in range(0, len(x)-(wind_size-sym_len), sym_len)])]
+    x = (x - np.mean(x))/np.std(x)
+    x_patched = x[np.array([idx + i for i in range(0, len(x), wind_size)])]
+
+    vecs_real = vecs[200:-200]
+    tmp = vecs_real.reshape(-1,2)
+    keys = np.unique(tmp, axis=0)
+    sym_l = np.zeros(tmp.shape[0])
+    for i, k in enumerate(keys):
+        sym_l[np.where((tmp == k).all(axis=1))] = i
 
 
-              assert False
-             
+    idx = np.arange(0, wind_size/sym_len)
+    # syms_patched = sym_l[np.array([idx + i for i in range(0, len(sym_l)-(wind_size/sym_len)+1, 1)])]
+    syms_patched = sym_l[np.array([idx + i for i in range(0, len(sym_l)-(wind_size/sym_len)+1, 16)])]
 
-print "all done. writing to disk"
-cPickle.dump( dataset, file("RML2016.10a_dict.dat", "wb" ) )
+    np.savez_compressed("../data_gen/data/4taps_stand%s_%d.npz" % (mod_type.modname,snr), x=x_patched, y=syms_patched)
+
+
